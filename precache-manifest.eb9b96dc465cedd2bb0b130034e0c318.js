b3aaa49a7d0251ef61a8315c56f0bc818002ea07
mmilic1994 <mmilic1994@gmail.com>
2019-07-01 22:45:02 +0200

    Updates

---
+++ precache-manifest.eb9b96dc465cedd2bb0b130034e0c318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ed5674e0fd309ae6ffd9d25574c33f",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "39d383f99a66b6da0f49",
+    "url": "/weather-app/static/css/main.8009a26e.chunk.css"
+  },
+  {
+    "revision": "9ab39bf724d219c5220e",
+    "url": "/weather-app/static/js/2.4ae0c7af.chunk.js"
+  },
+  {
+    "revision": "39d383f99a66b6da0f49",
+    "url": "/weather-app/static/js/main.bc0be042.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "2411d418743f92e0b5c5ab76e632648e",
+    "url": "/weather-app/static/media/DHMZlogo.2411d418.png"
+  },
+  {
+    "revision": "58f2f1fbfb935b5274743895f588488a",
+    "url": "/weather-app/static/media/openweather-logo.58f2f1fb.png"
+  }
+]);
